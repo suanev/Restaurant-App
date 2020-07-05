b8a72899a11b3a3927fef2700b6641d966ad1b11
Suane V <suanedarosav@gmail.com>
2020-07-05 15:55:41 -0300

    Validação customizada: email não repetido na inserção de CLiente

--- src/main/java/com/suanev/restaurant/Repositories/ClienteRepository.java
+++ src/main/java/com/suanev/restaurant/Repositories/ClienteRepository.java
@@ -4,6 +4,13 @@ import com.suanev.restaurant.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.transaction.annotation.Transactional;
+
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+
+    @Transactional(readOnly=true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/suanev/restaurant/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/suanev/restaurant/service/validation/ClienteInsertValidator.java
@@ -1,15 +1,22 @@
 package com.suanev.restaurant.service.validation;
 
+import com.suanev.restaurant.Repositories.ClienteRepository;
+import com.suanev.restaurant.domain.Cliente;
 import com.suanev.restaurant.domain.enums.TipoCliente;
 import com.suanev.restaurant.dto.ClienteNewDTO;
 import com.suanev.restaurant.resources.exceptions.FieldMessage;
 import com.suanev.restaurant.service.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -24,6 +31,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if(clienteDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(clienteDTO.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = clienteRepository.findByEmail(clienteDTO.getEmail());
+        if(aux != null) {
+            list.add(new FieldMessage("email", "Email já cadastrado."));
         }
 
         for (FieldMessage e : list) {
